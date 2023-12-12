c8577458cbacd680b0e14a8d4db89b2b50fe1514
Jasper Wong <wlcjasper@gmail.com>
2023-12-12 17:47:16 +0800

    add comments

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
+// two pages that will be shown to the user, depending on whether the URL has a fragment (i.e. something following the # sign)
 const entryPage = document.querySelector('.entry-page')
-const revealPage= document.querySelector('.reveal-page')
+const revealPage = document.querySelector('.reveal-page')
 
+// elements selected on the entry page (no fragment)
 const input = document.querySelector('#message-input')
 const btn = document.querySelector('.btn')
 const linkInput = document.querySelector('#link-input')
 
+// elements selected on the reveal page (yes fragment)
 const revealMessage = document.querySelector('#revealed-message')
 
 if (!`(${window.location}`.includes('#')) {
-// URL includes no query string
+  // URL includes no fragment
+  revealPage.style.display = 'none'
 
-  revealPage.style.display = 'none';
-   btn.addEventListener('click', event => {
+  // only encode the message when the button is clicked
+  btn.addEventListener('click', event => {
     event.preventDefault()
     const secretMessage = btoa(input.value)
-    linkInput.value = 
-    `${window.location}#${secretMessage}`
+    linkInput.value = `${window.location}#${secretMessage}`
     linkInput.select()
   })
 } else {
-  
-  entryPage.style.display = 'none';
-  const url = window.location.href;
-  const secretMessage = url.split('#')[1];
-  const plainMessage = atob(secretMessage);
+  // URL includes fragment
+  entryPage.style.display = 'none'
+  const url = window.location.href
+  const secretMessage = url.split('#')[1]
+  const plainMessage = atob(secretMessage)
 
-  console.log(plainMessage);
+  console.log(plainMessage)
 
-  revealMessage.innerHTML = `Your secret message is "${plainMessage}" <br>`;
+  revealMessage.innerHTML = `Your secret message is "${plainMessage}" <br>`
 }
